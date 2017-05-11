d62f00c9692061e29bf62b8b08773469e2b8b989
mahfuzt786 <mahfuzt786@yahoo.co.in>
2017-05-12 00:29:16 +0530

    js fixed..

--- major project/js/st.js
+++ major project/js/st.js
@@ -1,42 +1,70 @@
 $(document).ready(function(){
     $("div.row1").on("click","a#bt",function(){
         var v1,v2,v3,v4,v5,v6,v7,v8;
-        v1=$("div.row1","input#st").val();
-        v2=$("div.row1","input#ad").val();
-        v3=$("div.row1","input#na").val();
-        v4=$("div.row1","input#co").val();
-        v5=$("div.row1","div#aa").val();
-        v6=$("div.row1","textarea#pw").val();
-        v7=$("div.row1","input#cm").val();
-        v8=$("div.row1","input#em").val();
+        v1=$("div.row1 input#st_id").val();
+        v2=$("div.row1 input#admission").val();
+        v3=$("div.row1 input#name").val();
+        v4=$("div.row1 input#course").val();
+        v5=$("div.row1 textarea#address").val();
+        v6=$("div.row1 input#password").val();
+        v7=$("div.row1 input#contact").val();
+        v8=$("div.row1 input#email").val();
         if(v1===""){
-            alert("insert student id");
+            $('div#popupError div#successBody p.errorMessage').text('Please Enter Student ID');
+			$('div#popupError').popup("open");
         }
         else if(v2===""){
-            alert("insert admission no");
+            $('div#popupError div#successBody p.errorMessage').text('Please Enter Admission No');
+			$('div#popupError').popup("open");
         }
         else if(v3===""){
-            alert("insert name");
+            $('div#popupError div#successBody p.errorMessage').text('Please Enter Name');
+			$('div#popupError').popup("open");
         }
         else if(v4===""){
-            alert("insert name");
+            $('div#popupError div#successBody p.errorMessage').text('Please Enter Course');
+			$('div#popupError').popup("open");
         }
         else if(v5===""){
-            alert("insert name");
+            $('div#popupError div#successBody p.errorMessage').text('Please Enter Address');
+			$('div#popupError').popup("open");
         }
         else if(v6===""){
-            alert("insert name");
+            $('div#popupError div#successBody p.errorMessage').text('Please Enter Password');
+			$('div#popupError').popup("open");
         }
         else if(v7===""){
-            alert("insert name");
+            $('div#popupError div#successBody p.errorMessage').text('Please Enter Contact Number');
+			$('div#popupError').popup("open");
         }
         else if(v8===""){
-            alert("insert name");
+            $('div#popupError div#successBody p.errorMessage').text('Please Enter Email ID');
+			$('div#popupError').popup("open");
         }
+		else if(!IsEmail(v8))
+		{
+			$('div#popupError div#successBody p.errorMessage').text('Please enter email in xxxxx@xxx.xxx format. ie john.doe@gmail.com');
+			$('div#popupError').popup("open");
+		}
         else{
-            alert("submit");
-            $("div.row1","input#ad").val(v1);
+            $('div#popupError div#successBody p.errorMessage').text('Submit');
+			$('div#popupError').popup("open");
+			
+            $("div.row1 input#ad").val(v1);
         }
         
         });
+		
+		/*phone number check*/
+		$('#contact').keyup(function () {
+			if (!this.value.match(/^([0-9]{0,15})$/)) {
+				this.value = this.value.replace(/[^0-9]/g, '').substring(0,15);
+			}
+		});
+	   
+	   /*email validation*/
+		function IsEmail(email) {
+		var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		return regex.test(email);
+		}
     });
